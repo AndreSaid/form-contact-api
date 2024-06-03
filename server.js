6d0324ffb13dfd1702c58c4ecc41f99f3987c4de
AndreSaid <andresaid123@hotmail.com>
2024-06-03 11:01:52 -0300

    Configurar sendgrid para evitar problemas de bloqueio de ips

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log('EMAIL_USER:', process.env.EMAIL_USER); // Log para verificar a variável de ambiente
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? '*****' : 'Not Set'); // Log para verificar a variável de ambiente
-
-const transporter = nodemailer.createTransport({
-  service: 'hotmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -29,21 +20,23 @@ app.post('/api/contact', (req, res) => {
     return res.status(400).json({ error: 'Email inválido' });
   }
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
+  const msg = {
     to: process.env.EMAIL_USER, // Seu próprio email
+    from: process.env.EMAIL_USER,
     subject: 'Novo contato do formulário',
     text: `Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email enviado');
+      res.status(200).json({ message: 'Email enviado com sucesso!' });
+    })
+    .catch(error => {
       console.error('Erro ao enviar email:', error);
-      return res.status(500).json({ error: error.toString() });
-    }
-    console.log('Email enviado:', info.response);
-    res.status(200).json({ message: 'Email enviado com sucesso!', info: info.response });
-  });
+      res.status(500).json({ error: error.toString() });
+    });
 });
 
 app.listen(port, () => {
