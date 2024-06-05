46fa44c341ad4541b36e951dafa296c7683d71d1
AndreSaid <andresaid123@hotmail.com>
2024-06-05 09:08:54 -0300

    Retorno de resposta para o usuário

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? '*****' : 'Not Set');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -29,20 +27,35 @@ app.post('/api/contact', (req, res) => {
     return res.status(400).json({ error: 'Email inválido' });
   }
 
-  const mailOptions = {
+  const adminMailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_USER,
     subject: 'Novo contato do formulário',
-    text: `Nome: ${name}\nEmail: ${email}\nTelefone/WhatsApp: ${phone}\nMensagem: ${message}`
+    text: `Nome: ${name}\nEmail: ${email}\nTelefone: ${phone}\nMensagem: ${message}`
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  const userMailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: 'Confirmação de Recebimento de Contato',
+    text: `Olá ${name},\n\nRecebI sua mensagem:\n\n"${message}"\n\nEntrarei em contato assim que possivel.\n\nAtenciosamente,\nAndré Luís Said Domingues\nAnalista Desenvolvedor Senior`
+  };
+
+  transporter.sendMail(adminMailOptions, (error, info) => {
     if (error) {
-      console.error('Erro ao enviar email:', error);
+      console.error('Erro ao enviar email ao administrador:', error);
       return res.status(500).json({ error: error.toString() });
     }
-    console.log('Email enviado:', info.response);
-    res.status(200).json({ message: 'Email enviado com sucesso!', info: info.response });
+    console.log('Email enviado ao administrador:', info.response);
+
+    transporter.sendMail(userMailOptions, (error, info) => {
+      if (error) {
+        console.error('Erro ao enviar email ao usuário:', error);
+        return res.status(500).json({ error: error.toString() });
+      }
+      console.log('Email enviado ao usuário:', info.response);
+      res.status(200).json({ message: 'Emails enviados com sucesso!', info: info.response });
+    });
   });
 });
 
